be4d04e2ca68e843bb0225ab03e73282a930148b
Robyn Speer <rspeer@arborelia.net>
2012-08-23 15:32:16 -0400

    add code (mostly ported from metanl)

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="fixtext",
+    version = '1.0',
+    maintainer='Luminoso, LLC',
+    maintainer_email='dev@lumino.so',
+    license = "MIT",
+    url = 'http://github.com/LuminosoInsight/fixtext',
+    platforms = ["any"],
+    description = "Fixes some problems with Unicode text after the fact",
+    py_modules=['fixtext'],
+)
