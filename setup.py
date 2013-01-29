5d76608bca75e7829bd937786df84b4946fbcb44
Robyn Speer <rspeer@arborelia.net>
2013-01-29 15:19:28 -0500

    Update to version 2
    - command line utility
    - prevents DoS from long strings
    - faster code that uses Python built-ins for inner loops

--- setup.py
+++ setup.py
@@ -2,12 +2,17 @@ from setuptools import setup
 
 setup(
     name="ftfy",
-    version = '1.0',
+    version='2.0',
     maintainer='Luminoso, LLC',
     maintainer_email='dev@lumino.so',
-    license = "MIT",
-    url = 'http://github.com/LuminosoInsight/ftfy',
-    platforms = ["any"],
-    description = "Fixes some problems with Unicode text after the fact",
-    py_modules=['ftfy'],
+    license="MIT",
+    url='http://github.com/LuminosoInsight/ftfy',
+    platforms=["any"],
+    description="Fixes some problems with Unicode text after the fact",
+    packages=['ftfy'],
+    entry_points={
+        'console_scripts': [
+            'ftfy = ftfy.cli:main'
+        ]
+    }
 )
