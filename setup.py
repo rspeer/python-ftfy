340fc73705613c851681fec5193f3476a743289c
Robyn Speer <rspeer@arborelia.net>
2013-01-30 18:46:05 -0500

    add Trove classifiers; package as ftfy 2.0

--- setup.py
+++ setup.py
@@ -10,6 +10,19 @@ setup(
     platforms=["any"],
     description="Fixes some problems with Unicode text after the fact",
     packages=['ftfy'],
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: Filters",
+        "Development Status :: 5 - Production/Stable"
+    ],
     entry_points={
         'console_scripts': [
             'ftfy = ftfy.cli:main'
