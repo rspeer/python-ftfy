fc7f75119837378c033382f44a7603ef5b47266a
Robyn Speer <rspeer@arborelia.net>
2013-06-20 15:32:21 -0400

    bugfix release 2.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="ftfy",
-    version='2.0.1',
+    version='2.0.2',
     maintainer='Luminoso, LLC',
     maintainer_email='dev@lumino.so',
     license="MIT",
