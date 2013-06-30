f8c3daf1099e9ec99d0c39f186d517526d1f77c3
Robyn Speer <rspeer@arborelia.net>
2013-06-30 02:31:24 -0500

    WIP: simplify the logic and expand to more encodings

--- ftfy/chardata.py
+++ ftfy/chardata.py
@@ -3,47 +3,92 @@ from __future__ import unicode_literals
 
 import unicodedata
 import sys
+import re
+
 if sys.hexversion >= 0x03000000:
     unichr = chr
 
-# Start with an inventory of "gremlins", which are characters from all over
-# Unicode that Windows has instead assigned to the control characters
-# 0x80-0x9F. We might encounter them in their Unicode forms and have to figure
-# out what they were originally.
-
-WINDOWS_1252_GREMLINS = [
-    # adapted from http://effbot.org/zone/unicode-gremlins.htm
-    0x0152,  # LATIN CAPITAL LIGATURE OE
-    0x0153,  # LATIN SMALL LIGATURE OE
-    0x0160,  # LATIN CAPITAL LETTER S WITH CARON
-    0x0161,  # LATIN SMALL LETTER S WITH CARON
-    0x0178,  # LATIN CAPITAL LETTER Y WITH DIAERESIS
-    0x017E,  # LATIN SMALL LETTER Z WITH CARON
-    0x017D,  # LATIN CAPITAL LETTER Z WITH CARON
-    0x0192,  # LATIN SMALL LETTER F WITH HOOK
-    0x02C6,  # MODIFIER LETTER CIRCUMFLEX ACCENT
-    0x02DC,  # SMALL TILDE
-    0x2013,  # EN DASH
-    0x2014,  # EM DASH
-    0x201A,  # SINGLE LOW-9 QUOTATION MARK
-    0x201C,  # LEFT DOUBLE QUOTATION MARK
-    0x201D,  # RIGHT DOUBLE QUOTATION MARK
-    0x201E,  # DOUBLE LOW-9 QUOTATION MARK
-    0x2018,  # LEFT SINGLE QUOTATION MARK
-    0x2019,  # RIGHT SINGLE QUOTATION MARK
-    0x2020,  # DAGGER
-    0x2021,  # DOUBLE DAGGER
-    0x2022,  # BULLET
-    0x2026,  # HORIZONTAL ELLIPSIS
-    0x2030,  # PER MILLE SIGN
-    0x2039,  # SINGLE LEFT-POINTING ANGLE QUOTATION MARK
-    0x203A,  # SINGLE RIGHT-POINTING ANGLE QUOTATION MARK
-    0x20AC,  # EURO SIGN
-    0x2122,  # TRADE MARK SIGN
+# These are encodings that map each byte to a particular character.
+# They are listed in order of frequency, so that more frequent
+# encodings will be tried first.
+CHARMAP_ENCODINGS = [
+    'windows-1252',
+    'latin-1',
+    'macroman',
+    'cp437',
+    'windows-1251',
 ]
+CHARMAPS = {}
+ENCODING_REGULAR_EXPRESSIONS = {}
 
-# a list of Unicode characters that might appear in Windows-1252 text
-WINDOWS_1252_CODEPOINTS = list(range(256)) + WINDOWS_1252_GREMLINS
+for encoding in CHARMAP_ENCODINGS:
+    charmap = {}
+    for codepoint in range(0, 0x80):
+        charmap[unichr(codepoint)] = unichr(codepoint)
+    for codepoint in range(0x80, 0x100):
+        char = unichr(codepoint)
+        encoded_char = char.encode('latin-1')
+        try:
+            decoded_char = encoded_char.decode(encoding)
+        except ValueError:
+            decoded_char = char
+        charmap[decoded_char] = char
+
+    charlist = [char for char in sorted(charmap.keys()) if ord(char) >= 0x80]
+    regex = '^[\x00-\x7f{}]*$'.format(''.join(charlist))
+    CHARMAPS[encoding] = charmap
+    ENCODING_REGULAR_EXPRESSIONS[encoding] = re.compile(regex)
+
+ENCODING_REGULAR_EXPRESSIONS['ascii'] = re.compile('^[\x00-\x7f]*')
+
+
+# Challenge questions:
+# (1) How does this differ from the UTF-8 standard?
+# (2) Does it matter?
+
+UTF8_AS_BYTES = re.compile(
+    b'^('
+    b'[\x00-\x7f]|'
+    b'([\xc2-\xdf][\x80-\xbf])|'
+    b'(\xe0[\xa0-\xbf][\x80-\xbf])|'
+    b'([\xe1-\xef][\x80-\xbf][\x80-\xbf])|'
+    b'(\xf0[\x90-\xbf][\x80-\xbf][\x80-\xbf])|'
+    b'([\xf1-\xf4][\x80-\xbf][\x80-\xbf][\x80-\xbf])|'
+    b')*$'
+)
+
+def possible_encoding(text, encoding):
+    """
+    Given text and a single-byte encoding, check whether that text could have
+    been decoded from that single-byte encoding.
+    """
+    return bool(ENCODING_REGULAR_EXPRESSIONS[encoding].match(text))
+
+
+# ----------------
+
+
+# What is the heuristic for comparing single-byte encodings to each other?
+encodable_chars = set()
+for codepoint in range(0x80, 0x100):
+    encoded_char = chr(codepoint).encode('latin-1')
+    possibilities = []
+    for encoding in CHARMAP_ENCODINGS:
+        try:
+            decoded_char = encoded_char.decode(encoding)
+            encodable_chars.add(decoded_char)
+        except ValueError:
+            decoded_char = encoded_char.decode('latin-1')
+        possibilities.append(decoded_char)
+    print(' '.join(possibilities))
+print()
+
+for char in sorted(encodable_chars):
+    try:
+        name = unicodedata.name(char)
+    except ValueError:
+        name = '[unknown]'
+    print(char, hex(ord(char)), name)
 
 # Rank the characters typically represented by a single byte -- that is, in
 # Latin-1 or Windows-1252 -- by how weird it would be to see them in running
@@ -64,32 +109,112 @@ WINDOWS_1252_CODEPOINTS = list(range(256)) + WINDOWS_1252_GREMLINS
 # appear in the middle of a word in actual Portuguese, and meanwhile it
 # appears in the mis-encodings of many common characters.
 
-SINGLE_BYTE_WEIRDNESS = (
-#   0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f
-    5, 5, 5, 5, 5, 5, 5, 5, 5, 0, 0, 5, 5, 5, 5, 5,  # 0x00
-    5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,  # 0x10
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  # 0x20
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  # 0x30
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  # 0x40
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  # 0x50
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  # 0x60
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5,  # 0x70
-    2, 5, 1, 4, 1, 1, 3, 3, 4, 3, 1, 1, 1, 5, 1, 5,  # 0x80
-    5, 1, 1, 1, 1, 3, 1, 1, 4, 1, 1, 1, 1, 5, 1, 1,  # 0x90
-    1, 0, 2, 2, 3, 2, 4, 2, 4, 2, 2, 0, 3, 1, 1, 4,  # 0xa0
-    2, 2, 3, 3, 4, 3, 3, 2, 4, 4, 4, 0, 3, 3, 3, 0,  # 0xb0
-    0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  # 0xc0
-    1, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,  # 0xd0
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  # 0xe0
-    1, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0,  # 0xf0
-)
 
-# Pre-cache the Unicode data saying which of these first 256 characters are
-# letters. We'll need it often.
-SINGLE_BYTE_LETTERS = [
-    unicodedata.category(unichr(i)).startswith('L')
-    for i in range(256)
-]
+CHARACTER_WEIRDNESS = {
+    # \xa0 to \xaf
+    '\N{NO-BREAK SPACE}': 1,
+    '\N{INVERTED EXCLAMATION MARK}': 1,
+    '\N{CENT SIGN}': 2,
+    '\N{POUND SIGN}': 2,
+    '\N{CURRENCY SIGN}': 3,
+    '\N{YEN SIGN}': 2,
+    '\N{BROKEN BAR}': 4,
+    '\N{SECTION SIGN}': 2,
+    '\N{DIAERESIS}': 4,
+    '\N{COPYRIGHT SIGN}': 2,
+    '\N{FEMININE ORDINAL INDICATOR}': 3,
+    '\N{LEFT-POINTING DOUBLE ANGLE QUOTATION MARK}': 0,
+    '\N{NOT SIGN}': 3,
+    '\N{SOFT HYPHEN}': 1,
+    '\N{REGISTERED SIGN}': 1,
+    '\N{MACRON}': 4,
+
+    # \xb0 to \xbf
+    '\N{DEGREE SIGN}': 2,
+    '\N{PLUS-MINUS SIGN}': 2,
+    '\N{SUPERSCRIPT TWO}': 2,
+    '\N{SUPERSCRIPT THREE}': 3,
+    '\N{ACUTE ACCENT}': 4,
+    '\N{MICRO SIGN}': 3,
+    '\N{PILCROW SIGN}': 3,
+    '\N{MIDDLE DOT}': 2,
+    '\N{CEDILLA}': 4,
+    '\N{SUPERSCRIPT ONE}': 4,
+    '\N{MASCULINE ORDINAL INDICATOR}': 4,
+    '\N{RIGHT POINTING DOUBLE ANGLE QUOTATION MARK}': 0,
+    '\N{VULGAR FRACTION ONE QUARTER}': 2,
+    '\N{VULGAR FRACTION ONE HALF}': 2,
+    '\N{VULGAR FRACTION THREE QUARTERS}': 2,
+    '\N{INVERTED QUESTION MARK}': 0,
+
+    # \xc0 to \xff
+    '\N{LATIN CAPITAL LETTER A WITH TILDE}': 1,
+    '\N{LATIN CAPITAL LETTER ETH}': 1,
+    '\N{MULTIPLICATION SIGN}': 1,
+    '\N{LATIN SMALL LETTER ETH}': 1,
+    '\N{DIVISION SIGN}': 1,
+    '\N{LATIN CAPITAL LETTER Y WITH DIAERESIS}': 1,
+
+    # \U0100 to \U0200
+    '\N{LATIN CAPITAL LETTER Y WITH DIAERESIS}': 1,
+    '\N{LATIN SMALL LETTER F WITH HOOK}': 4,
+
+    # \U0200 to \U0300
+    '\N{MODIFIER LETTER CIRCUMFLEX ACCENT}': 1,
+    '\N{CARON}': 3,
+    '\N{BREVE}': 3,
+    '\N{DOT ABOVE}': 3,
+    '\N{RING ABOVE}': 3,
+    '\N{OGONEK}': 3,
+    '\N{SMALL TILDE}': 4,
+    '\N{DOUBLE ACUTE ACCENT}': 1,
+
+    # \U0300 to \U0400
+    '\N{GREEK CAPITAL LETTER GAMMA}': 1,
+    '\N{GREEK CAPITAL LETTER THETA}': 1,
+    '\N{GREEK CAPITAL LETTER SIGMA}': 1,
+    '\N{GREEK CAPITAL LETTER PHI}': 1,
+    '\N{GREEK CAPITAL LETTER OMEGA}': 1,
+    '\N{GREEK SMALL LETTER ALPHA}': 1,
+    '\N{GREEK SMALL LETTER DELTA}': 1,
+    '\N{GREEK SMALL LETTER EPSILON}': 1,
+    '\N{GREEK SMALL LETTER PI}': 1,
+    '\N{GREEK SMALL LETTER SIGMA}': 1,
+    '\N{GREEK SMALL LETTER TAU}': 1,
+    '\N{GREEK SMALL LETTER PHI}': 1,
+
+    # \U0400 to \U0500 is fine (Cyrillic)
+    # \U2000 to \U2400
+    '\N{DAGGER}': 2,
+    '\N{DOUBLE DAGGER}': 3,
+    '\N{PER MILLE SIGN}': 4,
+    '\N{FRACTION SLASH}': 2,
+    '\N{SUPERSCRIPT LATIN SMALL LETTER N}': 3,
+    '\N{PESETA SIGN}': 3,
+    '\N{NUMERO SIGN}': 2,
+    '\N{PARTIAL DIFFERENTIAL}': 1,
+    '\N{INCREMENT}': 1,
+    '\N{N-ARY PRODUCT}': 2,
+    '\N{N-ARY SUMMATION}': 2,
+    '\N{SQUARE ROOT}': 1,
+    '\N{INFINITY}': 1,
+    '\N{INTERSECTION}': 2,
+    '\N{INTEGRAL}': 2,
+    '\N{ALMOST EQUAL TO}': 1,
+    '\N{NOT EQUAL TO}': 1,
+    '\N{IDENTICAL TO}': 2,
+    '\N{LESS-THAN OR EQUAL TO}': 1,
+    '\N{GREATER-THAN OR EQUAL TO}': 1,
+    '\N{REVERSED NOT SIGN}': 4,
+    '\N{TOP HALF INTEGRAL}': 3,
+    '\N{BOTTOM HALF INTEGRAL}': 3,
+
+    # \U2500 to \U2600 are mostly box drawings; they're okay
+    '\N{LOZENGE}': 2,
+    '\uf8ff': 2,   # the Apple symbol
+    '\N{LATIN SMALL LIGATURE FI}': 1,
+    '\N{LATIN SMALL LIGATURE FL}': 1,
+}
 
 # Create a fast mapping that converts a Unicode string to a string describing
 # its character classes, particularly the scripts its letters are in.
@@ -172,10 +297,6 @@ SCRIPT_MAP[0x09] = ' '
 SCRIPT_MAP[0x0a] = '\n'
 SCRIPT_MAP[0xfffd] = '?'
 
-# mark weird extended characters as their own script
-for codepoint in range(0x100):
-    if SINGLE_BYTE_WEIRDNESS[codepoint] >= 2:
-        SCRIPT_MAP[codepoint] = 'W'
 
 # A translate mapping that will strip all control characters except \t and \n.
 # This incidentally has the effect of normalizing Windows \r\n line endings to
