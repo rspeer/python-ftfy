595539f7402ca9252a61c6f63243b15088d5a6a4
Robyn Speer <rspeer@arborelia.net>
2013-07-29 15:46:38 -0400

    added fix_surrogate_encoding fixer
    This allows the last test in test_real_tweets to pass, and not
    just by coincidence on narrow Python builds.

--- ftfy/test_real_tweets.py
+++ ftfy/test_real_tweets.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from ftfy.fixes import fix_text_encoding
+from ftfy import fix_text
 from nose.tools import eq_
 
 TEST_CASES = [
@@ -10,19 +10,32 @@ TEST_CASES = [
     #("Deja dos heridos hundimiento de barco tur\x92stico en Acapulco.",
     # "Deja dos heridos hundimiento de barco turístico en Acapulco."),
     ("âœ” No problems", "✔ No problems"),
-    ('4288×…', '4288×…'),
+    ('4288×…', '4288×...'),
     ("РґРѕСЂРѕРіРµ РР·-РїРѕРґ http://t.co/A0eJAMTuJ1 #С„СѓС‚Р±РѕР»",
      "дороге Из-под http://t.co/A0eJAMTuJ1 #футбол"),
-    ("Hi guys í ½í¸", "Hi guys 😍"),
     ("\x84Handwerk bringt dich \xfcberall hin\x93: Von der YOU bis nach Monaco",
-     '„Handwerk bringt dich überall hin“: Von der YOU bis nach Monaco')
+     '"Handwerk bringt dich überall hin": Von der YOU bis nach Monaco'),
+    ("Hi guys í ½í¸", "Hi guys 😍"),
 ]
 
 def test_real_tweets():
+    """
+    Test with text actually found on Twitter.
+
+    I collected these test cases by listening to the Twitter streaming API for
+    a million or so tweets, picking out examples with high weirdness according
+    to ftfy version 2, and seeing what ftfy decoded them to. There are some
+    impressive things that can happen to text, even in an ecosystem that is
+    supposedly entirely UTF-8.
+
+    The tweets that appear in TEST_CASES are the most interesting examples of
+    these, with some trickiness of how to decode them into the actually intended
+    text.
+    """
     for orig, target in TEST_CASES:
         # make sure we can decode the text as intended
-        eq_(fix_text_encoding(orig), target)
+        eq_(fix_text(orig), target)
 
         # make sure we can decode as intended even with an extra layer of badness
         extra_bad = orig.encode('utf-8').decode('latin-1')
-        eq_(fix_text_encoding(extra_bad), target)
+        eq_(fix_text(extra_bad), target)
