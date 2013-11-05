7d145b6090f8b15593505b4515937e6283bea611
Robyn Speer <rspeer@arborelia.net>
2013-11-05 18:23:04 -0500

    Version 3.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="ftfy",
-    version='3.0.5',
+    version='3.0.6',
     maintainer='Luminoso Technologies, Inc.',
     maintainer_email='dev@lumino.so',
     license="MIT",
