583a966b75175959f9933b7b10b3217d0d452bd5
Robyn Speer <rspeer@arborelia.net>
2013-11-05 18:22:49 -0500

    Add tests for the fix_entities parameter

---
+++ tests/test_entities.py
@@ -0,0 +1,18 @@
+from __future__ import unicode_literals
+from ftfy import fix_text, fix_text_segment
+from nose.tools import eq_
+
+def test_entities():
+    example = '&amp;\n<html>\n&amp;'
+    eq_(fix_text(example), '&\n<html>\n&amp;')
+    eq_(fix_text_segment(example), '&amp;\n<html>\n&amp;')
+
+    eq_(fix_text(example, fix_entities=True), '&\n<html>\n&')
+    eq_(fix_text_segment(example, fix_entities=True), '&\n<html>\n&')
+
+    eq_(fix_text(example, fix_entities=False), '&amp;\n<html>\n&amp;')
+    eq_(fix_text_segment(example, fix_entities=False), '&amp;\n<html>\n&amp;')
+
+    eq_(fix_text_segment('&lt;&gt;', fix_entities=False), '&lt;&gt;')
+    eq_(fix_text_segment('&lt;&gt;', fix_entities=True), '<>')
+    eq_(fix_text_segment('&lt;&gt;'), '<>')
