0532e7180c7dee8277a863f81b9d7cbd604474f1
Robyn Speer <rspeer@arborelia.net>
2013-11-27 12:19:21 -0500

    Output correct 'plans' and test them

--- ftfy/fixes.py
+++ ftfy/fixes.py
@@ -109,6 +109,7 @@ def fix_text_encoding(text):
 def fix_text_and_explain(text):
     best_version = text
     best_cost = text_cost(text)
+    best_plan = []
     plan_so_far = []
     while True:
         prevtext = text
@@ -130,8 +131,9 @@ def fix_text_and_explain(text):
         if cost < best_cost:
             best_cost = cost
             best_version = text
+            best_plan = list(plan_so_far)
         if text == prevtext:
-            return best_version, plan_so_far
+            return best_version, best_plan
 
 
 def fix_one_step_and_explain(text):
@@ -198,7 +200,9 @@ def fix_one_step_and_explain(text):
             encoded = text.encode('latin-1')
             try:
                 fixed = encoded.decode('windows-1252')
-                steps = [('encode', 'latin-1'), ('decode', 'windows-1252')]
+                steps = []
+                if fixed != text:
+                    steps = [('encode', 'latin-1'), ('decode', 'windows-1252')]
                 return fixed, steps
             except UnicodeDecodeError:
                 # This text contained characters that don't even make sense
