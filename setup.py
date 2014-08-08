297593b576148e2940a769ff0dcd1dca7974ec95
Robyn Speer <rspeer@arborelia.net>
2014-08-08 11:58:24 -0400

    update docs and version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="ftfy",
-    version='3.2.0',
+    version='3.3.0',
     maintainer='Luminoso Technologies, Inc.',
     maintainer_email='info@luminoso.com',
     license="MIT",
