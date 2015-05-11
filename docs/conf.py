5c79fb6e3d491ea76a70fecbb1a09ffff338b1be
Robyn Speer <rspeer@luminoso.com>
2015-05-11 14:54:41 -0400

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -94,7 +94,6 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
