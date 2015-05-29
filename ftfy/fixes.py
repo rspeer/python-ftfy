ac120c2559074517cac4bc9e37382061b0b1d5c8
Robyn Speer <rspeer@luminoso.com>
2015-05-29 11:29:39 -0400

    Explain `restore_byte_a0` better. Fixes #38.
    This is a change to a docstring only.

--- ftfy/fixes.py
+++ ftfy/fixes.py
@@ -565,9 +565,17 @@ def decode_escapes(text):
 
 def restore_byte_a0(byts):
     """
-    Find sequences that would convincingly decode as UTF-8 if the byte 0x20
-    were changed to 0xa0, and fix them. This is used as a step within
-    `fix_encoding`.
+    Some mojibake has been additionally altered by a process that said "hmm,
+    \\xa0, that's basically a space!" and replaced it with an ASCII space.
+    When the \\xa0 is part of a sequence that we intend to decode as UTF-8,
+    changing byte A0 to 20 would make it fail to decode.
+    
+    This process finds sequences that would convincingly decode as UTF-8 if
+    byte 20 were changed to A0, and puts back the A0. For the purpose of
+    deciding whether this is a good idea, this step gets a cost of twice
+    the number of bytes that are changed.
+    
+    This is used as a step within `fix_encoding`.
     """
     def replacement(match):
         "The function to apply when this regex matches."
