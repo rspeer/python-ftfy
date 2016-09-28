31fb8779656463a6eaf07c8750269dc4d8325ba4
Robyn Speer <rspeer@arborelia.net>
2016-09-28 11:40:55 -0400

    Bump version to 4.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 
 setup(
     name="ftfy",
-    version='4.1.2',
+    version='4.2.0',
     maintainer='Luminoso Technologies, Inc.',
     maintainer_email='info@luminoso.com',
     license="MIT",
