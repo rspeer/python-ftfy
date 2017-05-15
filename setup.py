d0af0567d9bec7a9ff11f1948deb0e4b8b05926f
Robyn Speer <rspeer@arborelia.net>
2017-05-15 17:14:03 -0400

    bump version to 5.1.1 and update the CHANGELOG

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ of it and write better, simpler code.
 The older version of ftfy, version 4.4, is still available and can run on
 Python 2. Try this:
 
-    pip install ftfy==4.4.2
+    pip install ftfy==4.4.3
 """
 
 
@@ -24,7 +24,7 @@ if sys.version_info[0] < 3:
 
 setup(
     name="ftfy",
-    version='5.1',
+    version='5.1.1',
     maintainer='Luminoso Technologies, Inc.',
     maintainer_email='info@luminoso.com',
     license="MIT",
