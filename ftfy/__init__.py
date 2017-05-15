832651c0170fbb6e07d5f8030a340d0111b4cf1d
Rob Speer <rob@luminoso.com>
2017-05-15 17:14:03 -0400

    bump version to 5.1.1 and update the CHANGELOG

--- ftfy/__init__.py
+++ ftfy/__init__.py
@@ -10,7 +10,7 @@ import ftfy.bad_codecs
 from ftfy import fixes
 from ftfy.formatting import display_ljust
 
-__version__ = '5.1'
+__version__ = '5.1.1'
 
 
 # See the docstring for ftfy.bad_codecs to see what we're doing here.
