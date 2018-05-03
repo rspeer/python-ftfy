773a09e38fda48f487c6c431af9704f5fbbedabc
Rob Speer <rob@luminoso.com>
2018-05-03 16:32:25 -0400

    replace enabled=False test cases with expected failures
    Now we can find out if something changes to make a test case
    unexpectedly pass.

--- tests/test_examples_in_json.py
+++ tests/test_examples_in_json.py
@@ -34,24 +34,43 @@ THIS_DIR = os.path.dirname(__file__)
 TEST_FILENAME = os.path.join(THIS_DIR, 'test_cases.json')
 TEST_DATA = json.load(open(TEST_FILENAME, encoding='utf-8'))
 
+TESTS_THAT_PASS = [test for test in TEST_DATA if test['expect'] == 'pass']
+TESTS_THAT_FAIL = [test for test in TEST_DATA if test['expect'] == 'fail']
+
 
 @pytest.mark.parametrize("test_case", TEST_DATA)
+def test_well_formed_example(test_case):
+    assert test_case['expect'] in ('pass', 'fail')
+
+
+@pytest.mark.parametrize("test_case", TESTS_THAT_PASS)
 def test_json_example(test_case):
     # Run one example from the data file
-    if test_case['enabled']:
-        orig = test_case['original']
-        fixed = test_case['fixed']
+    orig = test_case['original']
+    fixed = test_case['fixed']
 
-        # Make sure that the fix_encoding step outputs a plan that we can
-        # successfully run to reproduce its result
-        encoding_fix, plan = fix_encoding_and_explain(orig)
-        assert apply_plan(orig, plan) == encoding_fix
+    # Make sure that the fix_encoding step outputs a plan that we can
+    # successfully run to reproduce its result
+    encoding_fix, plan = fix_encoding_and_explain(orig)
+    assert apply_plan(orig, plan) == encoding_fix
 
-        # Make sure we can decode the text as intended
-        assert fix_text(orig) == fixed
-        assert encoding_fix == test_case.get('fixed-encoding', fixed)
+    # Make sure we can decode the text as intended
+    assert fix_text(orig) == fixed
+    assert encoding_fix == test_case.get('fixed-encoding', fixed)
 
-        # Make sure we can decode as intended even with an extra layer of badness
-        extra_bad = orig.encode('utf-8').decode('latin-1')
-        assert fix_text(extra_bad) == fixed
+    # Make sure we can decode as intended even with an extra layer of badness
+    extra_bad = orig.encode('utf-8').decode('latin-1')
+    assert fix_text(extra_bad) == fixed
+
+
+@pytest.mark.parametrize("test_case", TESTS_THAT_FAIL)
+@pytest.mark.xfail(strict=True)
+def test_failing_json_example(test_case):
+    # Run an example from the data file that we believe will fail, due to
+    # ftfy's heuristic being insufficient
+    orig = test_case['original']
+    fixed = test_case['fixed']
+
+    encoding_fix, plan = fix_encoding_and_explain(orig)
+    assert encoding_fix == test_case.get('fixed-encoding', fixed)
 
