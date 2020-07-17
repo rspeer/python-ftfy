33513948dcf5ba918ebd1e41cbf1fb6bc5ce635b
Robyn Speer <rspeer@arborelia.net>
2020-07-17 16:52:04 -0400

    update version numbers

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ copyright = u'2018, Robyn Speer'
 # built documents.
 #
 # The short X.Y version.
-version = '5.7'
+version = '5.8'
 # The full version, including alpha/beta/rc tags.
 release = '5.7'
 
