b5e3a68369f330564c559ed365f7dd6afae7c05c
Robyn Speer <rspeer@arborelia.net>
2021-03-22 13:31:50 -0400

    WIP: replace redundant heuristics

--- tests/test_characters.py
+++ tests/test_characters.py
@@ -26,18 +26,6 @@ def test_control_chars():
     assert remove_control_chars(text) == fixed
 
 
-def test_emoji_variation_selector():
-    # The hearts here are explicitly marked as emoji using the variation
-    # selector U+FE0F. This is not weird.
-    assert badness('❤\ufe0f' * 10) == 0
-
-
-def test_emoji_skintone_selector():
-    # Dear heuristic, you can't call skin-tone selectors weird anymore.
-    # We welcome Santa Clauses of all colors.
-    assert badness('🎅🏿🎅🏽🎅🏼🎅🏻') == 0
-
-
 def test_surrogates():
     assert fix_surrogates('\udbff\udfff') == '\U0010ffff'
     assert fix_surrogates('\ud800\udc00') == '\U00010000'
