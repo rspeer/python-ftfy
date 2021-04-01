606b37fea977c8f5451f52fa61453cc650c9882e
Robyn Speer <rspeer@luminoso.com>
2021-04-01 12:42:39 -0400

    more docs

--- docs/conf.py
+++ docs/conf.py
@@ -121,7 +121,7 @@ html_css_files = [
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = "ftfy v6"
+html_title = "ftfy: fixes text for you"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 html_short_title = "ftfy"
